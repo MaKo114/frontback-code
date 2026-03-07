65f7fd9cbd54be0563e712880d85ad179e16f968
tang <tang.legendza123@gmail.com>
2026-03-07 18:56:47 +0700

    Add favicon to tab-browser

--- client/src/titles/Title.ts
+++ client/src/titles/Title.ts
@@ -1,8 +1,14 @@
 import { useEffect } from 'react'
+import icon from "../../src/assets/logo/logo_tokladkrabang.png"
 
 const Title = () => {
     useEffect(()=>{
         document.title = "Tokladkrabang"
+        
+        const link = document.createElement("link");
+        link.rel = "icon";
+        link.href = icon;
+        document.head.appendChild(link);
     }, [])
 return null;
 }
